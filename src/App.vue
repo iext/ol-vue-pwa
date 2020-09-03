7fc9a535ad643df4acb9fd2ecceb1889d1c78dcb
alex <milickij@gmail.com>
2020-09-03 12:11:10 +0500

    Добавляет файлы для шаблона

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Map></Map>
+  </div>
+</template>
+
+<script>
+import Map from "./components/Map.vue";
+
+export default {
+  name: "App",
+  components: {
+    Map,
+  },
+};
+</script>
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
